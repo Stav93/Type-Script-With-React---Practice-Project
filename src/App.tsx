ea9aa551798a07467912afca6337d05c06b0585a
Stav <stavush93@gmail.com>
2022-11-22 22:33:24 +0200

    creating a components and props with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      
+      <Todos items={["Learn React", "Learn TypeScript"]} />
     </div>
   );
 }
